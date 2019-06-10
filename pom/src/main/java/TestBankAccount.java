2c37a99d69b908c6c4c891666d69b0cbb6b1e9e8
Vishesh51 <visheshsharma51@gmail.com>
2019-06-10 11:42:58 -0700

    Create TestBankAccount.java

---
+++ pom/src/main/java/TestBankAccount.java
@@ -0,0 +1,15 @@
+import junit.framework.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import Bank.BankAccount;
+
+public class TestBankAccount {
+  
+  @Test
+  public void testDebitWithSufficientFunds(){
+    BankAccount account = new BankAccount(10);
+    double amount=account.debit(5);
+    Assert.assertEquals(5.0,amount);
+  }
+
+}
