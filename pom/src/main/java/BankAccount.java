c6dea135560cb01b8d1d001422eb9543b65c6521
Vishesh51 <visheshsharma51@gmail.com>
2019-06-10 12:21:16 -0700

    Update BankAccount.java

--- pom/src/main/java/BankAccount.java
+++ pom/src/main/java/BankAccount.java
@@ -1,3 +1,8 @@
+import junit.framework.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+
 
 
 public class BankAccount {
@@ -15,6 +20,17 @@ public class BankAccount {
 
     balance -= amount;
     return amount;
+  }
+
+}
+
+public class TestBankAccount{
+  
+  @Test
+  public void testDebitWithSufficientFunds(){
+    BankAccount account = new BankAccount(10);
+    double amount=account.debit((double)5);
+    Assert.assertEquals(5.0,amount);
   }
 
 }
