d5b0cb73c195f6c9b5973e24416223e95164be9b
Vishesh51 <visheshsharma51@gmail.com>
2019-06-10 12:08:35 -0700

    Update TestBankAccount.java

--- pom/src/main/java/TestBankAccount.java
+++ pom/src/main/java/TestBankAccount.java
@@ -8,7 +8,7 @@ public class TestBankAccount  extends BankAccount {
   @Test
   public void testDebitWithSufficientFunds(){
     BankAccount account = new BankAccount(10);
-    double amount=account.debit(5);
+    double amount=account.debit((double)5);
     Assert.assertEquals(5.0,amount);
   }
 
